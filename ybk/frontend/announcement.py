784c91f8d6e3c7f04500d63f1e1fb7f60ef5bbe4
jingchao <jingchaohu@gmail.com>
2015-06-02 15:33:41 +0800

    add atom feed on announcement #6
    - raw url
    - http meta
    - rss icon
    - feed can do filter, too

--- ybk/frontend/announcement.py
+++ ybk/frontend/announcement.py
@@ -1,4 +1,5 @@
 from flask import render_template, request
+from werkzeug.contrib.atom import AtomFeed
 
 from ybk.models import Exchange, Announcement
 from ybk.utils import Pagination
@@ -10,6 +11,7 @@ from .views import frontend
 def announcement():
     nav = 'announcement'
     type_ = request.args.get('type', '')
+    typecn = '申购' if type_ == 'offer' else '中签'
     exchange = request.args.get('exchange', '')
     page = int(request.args.get('page', 1) or 1)
 
@@ -34,10 +36,44 @@ def announcement():
 
     try:
         updated_at = list(Exchange.find()
-                        .sort([('updated_at', -1)])
-                        .limit(1))[0].updated_at
+                          .sort([('updated_at', -1)])
+                          .limit(1))[0].updated_at
     except:
         # 只有当数据库为空时才会这样
         updated_at = None
 
     return render_template('frontend/announcement.html', **locals())
+
+
+@frontend.route('/announcement/feed.atom')
+def announcement_feed():
+    def bjdate(d):
+        from datetime import timedelta
+        return (d + timedelta(hours=8)).strftime('%Y年%m月%d日')
+
+    type_ = request.args.get('type', '')
+    exchange = request.args.get('exchange', '')
+    cond = {}
+    if type_:
+        cond['type_'] = type_
+    if exchange:
+        cond['exchange'] = exchange
+
+    feed = AtomFeed('最新公告',
+                    feed_url=request.url,
+                    url=request.url_root)
+
+    announcements = list(
+        Announcement.find(cond)
+        .sort([('published_at', -1)])
+        .limit(20))
+
+    for a in announcements:
+        feed.add('{} {}'.format(bjdate(a.published_at), a.title.strip()),
+                 '更多内容请点击标题连接',
+                 content_type='text',
+                 author=a.exchange,
+                 url=a.url,
+                 updated=a.published_at,
+                 published=a.published_at)
+    return feed.get_response()
