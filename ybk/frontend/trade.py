1da2e180fd93d79738200ad9501a2f5a6ec87fa9
jingchao <jingchaohu@gmail.com>
2015-07-03 18:33:45 +0800

    almost finish the trade page

--- ybk/frontend/trade.py
+++ ybk/frontend/trade.py
@@ -10,6 +10,12 @@ from ybk.models import Quote, Collection, Position
 @frontend.route('/trade/')
 def trade():
     nav = 'trade'
+    exchange = request.args.get('exchange')
+    symbol = request.args.get('symbol')
+    if exchange and symbol:
+        history_only = True
+    else:
+        history_only = False
     return render_template('frontend/trade.html', **locals())
 
 
@@ -38,36 +44,115 @@ def trade_quote_history():
     qs = list(Quote.cached(3600).find({'exchange': exchange,
                                        'symbol': symbol,
                                        'quote_type': period}))
-    xAxis = [{
-        'type': 'category',
-        'boundaryGap': True,
-        'axisTick': {'onGap': False},
-        'splitLine': {'show': False},
-        'data': [q.quote_at.strftime('%Y/%m/%d') for q in qs],
-    }]
-    series = \
-        [{
-            'name': '{}({}_{})'.format(c.name, c.exchange, c.symbol),
-            'type': 'k',
-            'data': [
-                (q.open_, q.high, q.low, q.close)
-                for q in qs
-            ]
-        }]
+
     name = '{}({}_{})'.format(Collection.get_name(exchange, symbol),
                               exchange, symbol)
-    title = {
-        'text': name
+    xdata = [q.quote_at.strftime('%Y/%m/%d') for q in qs]
+    sdata = [
+        (q.open_, q.high, q.low, q.close)
+        for q in qs]
+    adata = [int(q.amount / 10000) for q in qs]
+    num_prefix = 100 - len(xdata)
+    if num_prefix > 0:
+        xdata = xdata + [''] * num_prefix
+        sdata = sdata + [()] * num_prefix
+        adata = adata + [0] * num_prefix
+    option1 = {
+        'title': {
+            'text': name
+        },
+        'xAxis': [{
+            'type': 'category',
+            'boundaryGap': True,
+            'axisTick': {'onGap': False},
+            'splitLine': {'show': False},
+            'data': xdata,
+        }],
+        'legend': {
+            'data': [name, '成交额(万)']
+        },
+        'dataZoom': {
+            'show': False,
+            'realtime': True,
+            'start': (len(adata) - 100) * 100 // len(adata),
+            'end': 100
+        },
+        'series': [
+            {'name': '{}({}_{})'.format(c.name, c.exchange, c.symbol),
+             'type': 'k',
+             'data': sdata,
+             },
+            {'name': '成交额(万)',
+             'type': 'bar',
+             'symbol': 'none',
+             'data': [],
+             },
+        ],
     }
-    legend = {
-        'data': [name]
+
+    # chart2
+    option2 = {
+        'title': {
+            'text': '',
+        },
+        'toolbox': {
+            'y': -30,
+            'show': True,
+            'feature': {
+                'mark': {'show': True},
+                'dataZoom': {'show': True},
+                'dataView': {'show': True, 'readOnly': False},
+                'magicType': {'show': True, 'type': ['line', 'bar']},
+                'restore': {'show': True},
+                'saveAsImage': {'show': True}
+            }
+        },
+        'tooltip': {
+            'trigger': 'axis',
+            'showDelay': 0,
+        },
+        'legend': {
+            'y': -30,
+            'data': ['成交额(万)'],
+        },
+        'xAxis': [
+            {
+                'type': 'category',
+                'position': 'top',
+                'boundaryGap': True,
+                'axisLabel': {'show': False},
+                'axisTick': {'onGap': False},
+                'splitLine': {'show': False},
+                'data': xdata,
+            }
+        ],
+        'yAxis': {
+            'splitNumber': 3,
+        },
+        'series': [
+            {
+                'name': '成交额(万)',
+                'type': 'bar',
+                'symbol': 'none',
+                'data': adata,
+            }
+        ],
+        'grid': {
+            'x': 80,
+            'y': 5,
+            'x2': 20,
+            'y2': 40
+        },
+        'dataZoom': {
+            'show': True,
+            'realtime': True,
+            'start': (len(adata) - 100) * 100 // len(adata),
+            'end': 100
+        },
     }
     return jsonify(status=200,
-                   series=series,
-                   xAxis=xAxis,
-                   title=title,
-                   legend=legend
-                   )
+                   option1=option1,
+                   option2=option2)
 
 
 def trade_quote_realtime():
@@ -76,44 +161,78 @@ def trade_quote_realtime():
     # 指数 -> index
     # 持仓 -> position
     # 自选 -> diy
-    category = request.args.get('category')
+    category = request.args.get('category', '').strip()
+    search = request.args.get('search', '').strip()
+    sort = request.args.get('sort', '').strip()
+    order = request.args.get('order', 'asc').strip()
+
     today = Quote.cached(3600).find_one({'quote_type': '1d'},
                                         sort=[('quote_at', -1)]).quote_at
-    if category == 'all':
-        qs = list(Quote.find({'quote_type': '1d',
-                              'quote_at': today}))
+    cond = {'quote_type': '1d',
+            'quote_at': today}
+    colls = set()
+    symbols = []
+    if search:
+        cs = list(Collection.cached(3600).find(
+            {}, {'exchange': 1, 'name': 1, 'symbol': 1}))
+        cs = [c for c in cs
+              if search in c.exchange or
+              search in c.symbol or
+              search in c.name]
+        symbols = [c.symbol for c in cs]
+        colls = set((c.exchange, c.symbol) for c in cs)
+        cond['symbol'] = {'$in': symbols}
+    elif category == 'all':
+        pass
     elif category == 'index':
         cs = list(Collection.cached(3600).find({'name': {'$regex': '指数$'}}))
         colls = set((c.exchange, c.symbol) for c in cs)
         symbols = [c.symbol for c in cs]
-        qs = [q for q in Quote.find({'quote_type': '1d',
-                                     'quote_at': today,
-                                     'symbol': {'$in': symbols}})
-              if (q.exchange, q.symbol) in colls]
+        cond['symbol'] = {'$in': symbols}
     elif category == 'position':
         position = Position.user_position(current_user._id)
         colls = set((p['exchange'], p['symbol']) for p in position)
         symbols = [p['symbol'] for p in position]
-        qs = [q for q in Quote.find({'quote_type': '1d',
-                                     'quote_at': today,
-                                     'symbol': {'$in': symbols}})
-              if (q.exchange, q.symbol) in colls]
+        cond['symbol'] = {'$in': symbols}
     elif category == 'diy':
         raise NotImplementedError
 
+    qs = [q for q in Quote.find(cond)
+          if (not colls) or
+          ((q.exchange, q.symbol) in colls)]
+
+    qs = [{
+        'open_': q.open_,
+        'high': q.high,
+        'low': q.low,
+        'close': q.close,
+        'lclose': q.lclose,
+        'volume': q.volume,
+        'amount': q.amount,
+        'increase': 0 if not q.lclose else q.close / q.lclose - 1,
+        'exchange': q.exchange,
+        'symbol': q.symbol,
+        'name': Collection.get_name(q.exchange, q.symbol),
+    } for q in qs]
+
+    # sort
+    if sort:
+        qs = sorted(qs,
+                    key=lambda x: x[sort],
+                    reverse=order == 'desc')
+
+    # format
+    for q in qs:
+        q['open_'] = '{:.2f}'.format(q['open_'])
+        q['high'] = '{:.2f}'.format(q['high'])
+        q['low'] = '{:.2f}'.format(q['low'])
+        q['close'] = '{:.2f}'.format(q['close'])
+        q['amount'] = '{:.1f}万'.format(q['amount'] / 10000)
+        q['increase'] = '{:.1f}%'.format(q['increase'] * 100)
+
     return jsonify(status=200,
-                   quotes=[{
-                       'open_': q.open_,
-                       'high': q.high,
-                       'low': q.low,
-                       'close': q.close,
-                       'lclose': q.lclose,
-                       'volume': q.volume,
-                       'amount': q.amount,
-                       'exchange': q.exchange,
-                       'symbol': q.symbol,
-                       'name': Collection.get_name(q.exchange, q.symbol),
-                   } for q in qs])
+                   total=len(qs),
+                   rows=qs)
 
 
 def trade_quote_daytrade():
