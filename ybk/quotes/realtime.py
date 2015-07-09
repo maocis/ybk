a7df6c67013f05bcd374f2f58236cd0ee7e8c1e4
jingchao <jingchaohu@gmail.com>
2015-07-09 12:42:03 +0300

    remove -6

--- ybk/quotes/realtime.py
+++ ybk/quotes/realtime.py
@@ -54,7 +54,7 @@ def realtime(site):
     for q in quotes:
         coll = Collection._get_collection()
         coll.update({'exchange': exchange,
-                     'symbol': q['symbol'].strip()[-6:]},
+                     'symbol': q['symbol'].strip()},
                     {'$set': {'name': q['name']}},
                     upsert=True)
         q['exchange'] = exchange
