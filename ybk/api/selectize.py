06a4139aa6cab773b41149775966d125ba5440c1
jingchao <jingchaohu@gmail.com>
2015-09-04 07:05:22 +0800

    clear 下单选择

--- ybk/api/selectize.py
+++ ybk/api/selectize.py
@@ -8,6 +8,7 @@ from .views import api
 def load_symbols():
     exchange = request.args.get('exchange', '')
     query = request.args.get('query', '')
+    exclude = request.args.get('exclude', '')
     cond = {'$or': [{'name': {'$regex': query}},
                     {'symbol': {'$regex': query}}]}
     if exchange:
@@ -18,4 +19,6 @@ def load_symbols():
         for c in Collection.query(
             cond,
             {'name': 1, 'symbol': 1})]
+    if exclude:
+        result = [r for r in result if exclude not in r['text']]
     return jsonify(result=result)
